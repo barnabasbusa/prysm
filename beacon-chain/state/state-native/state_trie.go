4d2fe4334e2735d485f3b6ac1ad9ab2c24d56fc3
Bastin <43618253+Inspector-Butters@users.noreply.github.com>
2026-07-24 11:34:52 +0200

    progressive hashTreeRoot (#16861)
    progressive merklization of beacon state container. 
    
    note: 
    this implementation is naive and it's ignoring all the partial rebuilds.
    the goal is to have a correct implementation and then optimize it.
    
    ---------
    
    Co-authored-by: Jun Song <87601811+syjn99@users.noreply.github.com>

--- beacon-chain/state/state-native/state_trie.go
+++ beacon-chain/state/state-native/state_trie.go
@@ -1100,6 +1100,11 @@ func (b *BeaconState) HashTreeRoot(ctx context.Context) ([32]byte, error) {
 
 	b.lock.Lock()
 	defer b.lock.Unlock()
+
+	if features.ProgressiveSSZEnabled(b.version) {
+		return b.progressiveHashTreeRoot(ctx)
+	}
+
 	if err := b.initializeMerkleLayers(ctx); err != nil {
 		return [32]byte{}, err
 	}
@@ -1107,6 +1112,29 @@ func (b *BeaconState) HashTreeRoot(ctx context.Context) ([32]byte, error) {
 		return [32]byte{}, err
 	}
 	return bytesutil.ToBytes32(b.merkleLayers[len(b.merkleLayers)-1][0]), nil
+}
+
+func (b *BeaconState) progressiveHashTreeRoot(ctx context.Context) ([32]byte, error) {
+	fieldRoots, err := ComputeFieldRootsWithHasher(ctx, b)
+	if err != nil {
+		return [32]byte{}, errors.Wrap(err, "failed to compute state field roots")
+	}
+
+	progressiveFieldRoots := make([][32]byte, len(fieldRoots))
+	for i, fieldRoot := range fieldRoots {
+		progressiveFieldRoots[i] = bytesutil.ToBytes32(fieldRoot)
+	}
+
+	activeFields := make([]bool, len(fieldRoots))
+	for i := range activeFields {
+		activeFields[i] = true
+	}
+
+	root, err := ssz.ContainerRootProgressive(progressiveFieldRoots, activeFields)
+	if err != nil {
+		return [32]byte{}, fmt.Errorf("could not compute progressive container root: %w", err)
+	}
+	return root, nil
 }
 
 // Initializes the Merkle layers for the beacon state if they are empty.
